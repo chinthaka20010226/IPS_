d731521f6b5581c793dbdb7a094149f373d83b46
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-08 19:00:17 +0530

    Variables Declaration & Initialization

--- java/day_01/Example.java
+++ java/day_01/Example.java
@@ -130,14 +130,148 @@
 // ====================================================================================
 // .println() VS .print()
 
-public class Example{
-    public static void main(String args[]){
-        System.out.println("A");
-        System.out.println("B");
-        System.out.print("C");
-        System.out.println();
-        //System.out.print();  // Error,value is mandatory inside the bracket, 
-        System.out.print("D");
-        System.out.println("E");
-    }
-}
+// public class Example{
+//     public static void main(String args[]){
+//         System.out.println("A");
+//         System.out.println("B");
+//         System.out.print("C");
+//         System.out.println();
+//         //System.out.print();  // Error,value is mandatory inside the bracket, 
+//         System.out.print("D");
+//         System.out.println("E");
+//     }
+// }
+
+
+// =============================================================================================
+// Variables
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;      // variable declaration
+//         x = 100;    // variable initialization
+//         System.out.println(x);  // print
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         x = 100;
+//         System.out.println("x");
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         x = 100;
+//         System.out.println((int)'x');
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         // x = 100;
+//         System.out.println(x); // given Error
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         System.out.println(x); // given Error
+//         x = 100;
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         x = 100;
+//         x = 200;
+//         System.out.println(x);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x;
+//         x = 100;
+//         System.out.println(x);
+//         x = 200;
+//         System.out.println(x);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x = 100;
+//         System.out.println(x);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x = 100;
+//         System.out.println(x);
+
+//         int x = 200;
+//         System.out.println(x);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x,y,z;
+//         x = 100;
+//         y = 200;
+//         z = 300;
+//         System.out.println(x);
+//         System.out.println(y);
+//         System.out.println(z);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x=100,y=200,z=300;
+//         System.out.println(x);
+//         System.out.println(y);
+//         System.out.println(z);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x,y,z;
+//         x=y=z=100;
+//         System.out.println(x);
+//         System.out.println(y);
+//         System.out.println(z);
+//     }
+// }
+
+
+// public class Example{
+//     public static void main(String args[]){
+//         int x=y=z=300;
+//         System.out.println(x);
+//         System.out.println(y);
+//         System.out.println(z);
+//     }
+// }
+
+
