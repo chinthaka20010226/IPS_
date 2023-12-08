0b5132743aba5b9d1480c28e3414fdc1e12ccf6c
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-08 17:46:00 +0530

    main method declaraton

--- java/day_01/Example.java
+++ java/day_01/Example.java
@@ -1,5 +1,86 @@
-public class Example{
-    public static void main(String args[]){
-        System.out.println("Hello Java!");
-    }
-}
+// public class Example{
+//     public static void main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+// class Demo{
+//     public static void main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+// ===============================================================================================
+// ---> main method declaration,
+
+// Legal & Valid 
+
+// public class Example{
+//     public static void main(String[] args){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     public static void main(String abc[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     static public void main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+// Legal & Invalid (No problem in Compiled, but problem in Run time)
+
+// public class Example{
+//     public static void main(String args){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     public static void main(){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     static void main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     public static void Main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+// Illegal (Not compiled)
+
+// public class Example{
+//     public static main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     public void static main(String args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+// public class Example{
+//     public static void main(string args[]){
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
