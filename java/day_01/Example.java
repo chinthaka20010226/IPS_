9f6d8a9d66d11a3b0a66bdf76d644ed28e39a87f
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-09 18:05:29 +0530

    break, continue.....

--- java/day_01/Example.java
+++ java/day_01/Example.java
@@ -1024,3 +1024,27 @@ import java.util.*;
 //         }while(i<3);
 //     }
 // }
+
+
+// ============================================================================================
+
+// --> break, continue
+
+public class Example{
+    public static void main(String args[]){
+        // for(int i=0;i<10;i++){
+        //     if(i==5){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+
+        for(int i=0;i<10;i++){
+            if(i==5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
