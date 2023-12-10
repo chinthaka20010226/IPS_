67e155c05ced02603e234b3f2b26fd1dfcf62944
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-12-10 13:58:29 +0530

    2D-Array...

--- java/day_01/Example.java
+++ java/day_01/Example.java
@@ -1805,16 +1805,67 @@ import java.util.*;
 
 // ---> for-each lopp...
 
+// public class Example{
+//     public static void main(String args[]){
+//         int[] arr=new int[3];
+
+//         arr[0]=1;
+//         arr[1]=2;
+//         arr[2]=3;
+
+//         for(int x:arr){
+//             System.out.println(x);
+//         }
+//     }
+// }
+
+
+
+
+// ---> 2D Array...
+
 public class Example{
     public static void main(String args[]){
-        int[] arr=new int[3];
+        // int[][] arr=new int[5][3];
+        // // System.out.println(arr[0][0]);
 
-        arr[0]=1;
-        arr[1]=2;
-        arr[2]=3;
+        
+        // int[][] arr=new int[5][];
+        // arr[0]=new int[3];
+        // arr[1]=new int[4];
+        // arr[2]=new int[2];
+        // arr[3]=new int[5];
+        // arr[4]=new int[3];
+        // System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.deepToString(arr));
+        // System.out.println(Arrays.toString(arr[0]));
+        // System.out.println(Arrays.toString(arr[1]));
+        // System.out.println(Arrays.toString(arr[2]));
 
-        for(int x:arr){
-            System.out.println(x);
+        // arr[3][3]=11;
+        // System.out.println(Arrays.toString(arr[3]));
+
+
+
+        Random random=new Random();
+        int[][] arr=new int[5][3];
+
+        // insert values...
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]=random.nextInt(100);
+            }
         }
+
+        // print values...
+        System.out.print("[");
+        for(int i=0;i<arr.length;i++){
+            System.out.print("[");
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+", ");
+            }
+            System.out.print("\b\b]"+", ");
+        }
+        System.out.println("\b\b]");
     }
 }
